6ec8abb7cc8a39549f6804fbd549740d6f6c2b34
Andreansx <koliberekart@icloud.com>
2025-06-29 18:21:42 +0200

    now it hides the bar also when window is maximized by smart_gaps

--- polybar/bar-hider.sh
+++ polybar/bar-hider.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-is_active_window_fullscreen() {
+is_active_window_maximized() {
     if ! command -v xdotool &> /dev/null; then
-        echo "Error: xdotools is not installed. Run: 'sudo pacman -S xdotool'"
+        echo "Error: xdotool is not installed. Run: 'sudo pacman -S xdotool'"
         return 1
     fi
     
     active_win_id=$(xdotool getactivewindow)
+
+    if [ -z "$active_win_id" ]; then
+        return 1
+    fi
     
-    xprop -id "$active_win_id" _NET_WM_STATE | grep -q "_NET_WM_STATE_FULLSCREEN"
+    local properties
+    properties=$(xprop -id "$active_win_id" _NET_WM_STATE)
+    
+    if echo "$properties" | grep -q "_NET_WM_STATE_FULLSCREEN"; then
+        return 0     
+    elif echo "$properties" | grep "MAXIMIZED_HORZ" | grep -q "MAXIMIZED_VERT"; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 toggle_polybar() {
-    if is_active_window_fullscreen; then
+    if is_active_window_maximized; then
         polybar-msg cmd hide
     else
         polybar-msg cmd show
